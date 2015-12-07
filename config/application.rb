079edb9a8ce13994f37778cf17ebe12bd02c1808
Martin Ben <mebenav88@gmail.com>
2015-12-07 15:19:16 -0500

    add true to serve static assets

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,8 @@ module Collegeperks
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
+    config.serve_static_assets = true
     # config.assets.paths << "#{Rails.root}/app/assets/video"
     # config.assets.enabled = true
   end
