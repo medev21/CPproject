246ae4ec7dc1a944d3f5f5b496a2b95fd918b8c8
Martin Ben <mebenav88@gmail.com>
2015-12-07 17:14:43 -0500

    add asset pipeline

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,9 +15,14 @@ protect_from_forgery with: :exception
 skip_before_action :verify_authenticity_token
 before_action :setup_mcapi
 
+# def setup_mcapi
+#   @mc = Mailchimp::API.new(Rails.application.secrets.mailchimp_api_key)
+#   @list_id = Rails.application.secrets.mailchimp_list_id
+# end
+
 def setup_mcapi
-  @mc = Mailchimp::API.new(Rails.application.secrets.mailchimp_api_key)
-  @list_id = Rails.application.secrets.mailchimp_list_id
+  @mc = Mailchimp::API.new('18ab32802f86963f173410ffa7241d44-us1')
+  @list_id = "023ca95f64"
 end
 
 end
