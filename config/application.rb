123ac64ca2f8227c0f96e5d8d551f0b9d32ff55c
Martin Ben <mebenav88@gmail.com>
2015-12-07 15:13:57 -0500

    add initialize on precompile code to applications rb

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Collegeperks
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
+    config.assets.initialize_on_precompile = false
     # config.assets.paths << "#{Rails.root}/app/assets/video"
     # config.assets.enabled = true
   end
